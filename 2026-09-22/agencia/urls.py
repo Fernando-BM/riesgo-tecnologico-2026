6c810d3e7fe38cb99ef161b3e21fcd8a69acb7c9
Víctor <vf2k13@gmail.com>
2026-09-22 09:22:13 -0600

    últimos arreglos

--- 2026-09-22/agencia/urls.py
+++ 2026-09-22/agencia/urls.py
@@ -5,6 +5,9 @@ app_name = 'agencia'
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
+    #Url's parametrizadas <TIPO:NOMBRE_VAR>
+    #servicios principales menú superior
     path('<str:servicio>/', views.seccion, name='seccion'),
+    #servicios secundarios del menú superior
     path('<str:servicio>/<str:subtema>/', views.detalle, name='detalle'),
 ]
--- 2026-09-22/agencia/views.py
+++ 2026-09-22/agencia/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
-# Diccionario que define A (llaves) y B (listas)
+# Diccionario que define A (llaves) y B (listas/valores).
+#B se puede ver como si fueran subtemas
+#tema1': ['subtema1.1', 'subtema1.2', 'subtema1.3'],
+#tema2': ['subtema2.1', 'subtema2.2', 'subtema2.3'],
 MENU = {
     'Ventas': ['Nuevos', 'Seminuevos', 'Garantias'],
     'Mecanico': ['Mantenimiento', 'Motor', 'Frenos'],
@@ -8,19 +11,56 @@ MENU = {
     'Hojalateria': ['Pintura', 'Abolladuras']
 }
 
+#{% url 'agencia:inicio' %}
 def inicio(request):
     return render(request, 'agencia/inicio.html', {'menu_a': MENU.keys()})
 
+#Vamos a recibir como parámetro de la url la var servicio
 def seccion(request, servicio):
+    #Obtenemos el listado de subtemas
     opciones_b = MENU.get(servicio, [])
     contexto = {
         'menu_a': MENU.keys(),
+        #Metemos al contexto el servicio secundario (subtema)
         'servicio_activo': servicio,
+        #También mandamos la lista de servicios secundarios (subtemas)
         'opciones_b': opciones_b
     }
+    #Respondemos a la petición con el .html y el contexto
     return render(request, 'agencia/inicio.html', contexto)
 
+#Función que regresa un html dependiendo del subtema seleccionado
+def obtener_html_subtema(subtema):
+    match subtema:
+        case 'Nuevos':
+            return 'agencia/detalle.html'
+        case 'Seminuevos':
+            return 'agencia/algun.html'
+        case 'Garantias':
+            return 'agencia/algun.html'
+        case 'Mantenimiento':
+            return 'agencia/algun.html'
+        case 'Motor':
+            return 'agencia/algun.html'
+        case 'Frenos':
+            return 'agencia/algun.html'
+        case 'Aceites':
+            return 'agencia/algun.html'
+        case 'Filtros':
+            return 'agencia/algun.html'
+        case 'Llantas':
+            return 'agencia/algun.html'
+        case 'Pintura':
+            return 'agencia/algun.html'
+        case 'Abolladuras':
+            return 'agencia/algun.html'
+        case _:
+            return 'agencia/error.html'
+
+
+#Vamos a recibir como parámetro de la url las vars servicio y subtema
 def detalle(request, servicio, subtema):
+    #Obtenemos el listado de subtemas
     opciones_b = MENU.get(servicio, [])
     contexto = {
         'menu_a': MENU.keys(),
@@ -28,4 +68,10 @@ def detalle(request, servicio, subtema):
         'opciones_b': opciones_b,
         'subtema_activo': subtema,
     }
+
     return render(request, 'agencia/detalle.html', contexto)
+    #Actividad
+    '''
+    respuesta_html = obtener_html_subtema(subtema)
+    return render(request, respuesta_html, contexto)
+    '''
